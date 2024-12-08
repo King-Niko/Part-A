e402a83d8d1c84e2dae6e085ceb783c03463814e
King-Niko <nickd_walling!@yahoo.com>
2024-12-08 18:02:28 -0500

    Add project files.

---
+++ Part A/Part A.cpp
@@ -0,0 +1,20 @@
+// Nicholas Walling
+// CIS 1202 101
+// December 8, 2024
+// Exceptions, Templates, & STL Part A
+// A program that uses try/catch blocks using the stdexception library to call a function
+// with different starts and offsets.
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+char character(char start, int offset)
+{
+
+}
+
+int main()
+{
+    // Driver
+}
